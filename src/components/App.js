f370e95da27447a6f254f10536bdda7d70f87157
usmangillpro <usmangillpro@gmail.com>
2020-11-13 12:03:08 +0500

    Video details component added and adjusted, ui adjusted of the app

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,17 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
   state = {
     videos: [],
+    selectedVideo: null,
   };
+
+  componentDidMount() {
+    this.onTermSubmit("buildings");
+  }
 
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -14,14 +20,34 @@ class App extends React.Component {
         q: term,
       },
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onSearch={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
